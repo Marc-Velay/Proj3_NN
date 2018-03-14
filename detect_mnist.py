e05a3bb1970d93d1b65742548260dcf475faf842
Marc-Velay <marc.velay@gmail.com>
2018-03-14 19:03:09 +0100

    digit detection, generating new images, training default network training

--- detect_mnist.py
+++ detect_mnist.py
@@ -3,11 +3,14 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
 import numpy as np
+from numpy import transpose
 from random import shuffle, randrange
 import pickle
 import os.path
+import random
+import itertools
 
-mlp_file = 'saved_classifier.pkl'
+mlp_file = 'saved_classifier_detection.pkl'
 
 def load_data(dtype=np.float32, order='F'):
     """Load the data, then train/test split"""
@@ -27,58 +30,69 @@ def load_data(dtype=np.float32, order='F'):
 
     return X_train, X_test, y_train, y_test
 
+def create_imgs(num_img):
+    generated_samples = []
+
+    for gen in range(0, num_img):
+        temp = []
+        sample = np.random.randint(2, size=784)
+        temp.append(sample)
+        #print(len(temp))
+        #print(temp)
+        generated_samples.append(temp[0])
+    generated_samples = np.matrix(generated_samples)
+
+    return generated_samples
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = load_data()
 
+    new_samples = create_imgs(30000)
+    sampled_mnist = X_train[::2]
+    X_train = np.concatenate((sampled_mnist, new_samples), axis=0)
+    Y_train = []
+    Y_train.extend(np.ones((1,30000))[0])
+    Y_train.extend(np.zeros((1,30000))[0])
+    Y_train = np.array(Y_train)
+    Y_train = np.reshape(Y_train, (60000,1))
+    X_train, Y_train = unison_shuffled_copies(X_train, Y_train)
+    print(Y_train.shape)
 
-    '''for i in range(0,5):
-        ex = np.reshape(X_train[i*10000], (28,28))
-        plt.imshow(ex)
-        plt.show()
-        print(y_train[i*10000])
-        '''
+    new_samples_test = create_imgs(10000)
+    X_test = np.concatenate((X_test, new_samples_test), axis=0)
+    Y_test = []
+    Y_test.extend(np.ones((1,10000))[0])
+    Y_test.extend(np.zeros((1,10000))[0])
+    Y_test = np.array(Y_test)
+    Y_test = np.reshape(Y_test, (20000,1))
+    X_test, Y_test = unison_shuffled_copies(X_test, Y_test)
+    print(Y_test.shape)
+
     if not os.path.isfile(mlp_file):
         mlp = MLPClassifier(hidden_layer_sizes=(80, 40, 15), activation='logistic', solver='lbfgs', learning_rate_init=1e-4)
         print('Training neural network')
-        mlp.fit(X_train, y_train)
+        mlp.fit(X_train, Y_train.ravel())
 
         output = mlp.predict(X_test)
 
-        score = mlp.score(X_test, y_test)
+        score = mlp.score(X_test, Y_test.ravel())
         print('Accuracy: ')
         print(score)
 
-        conf_mat = confusion_matrix(y_test, output)
+        conf_mat = confusion_matrix(Y_test.ravel(), output)
         print('Confusion matrix: ')
         print(conf_mat)
 
-        with open(mlp_file, 'wb') as fid:
-            pickle.dump(mlp, fid)
-    else:
+        #with open(mlp_file, 'wb') as fid:
+            #pickle.dump(mlp, fid)
+        #    pass
+    '''else:
         print('already trained network, delete .pkl file to retrain')
         with open(mlp_file, 'rb') as fid:
             mlp = pickle.load(fid)
-
-    for i in range(0, 10):
-        index = randrange(0, 60000, 1)
-        pred = mlp.predict(X_train[index:index+1])
-        pred_proba = mlp.predict_proba(X_train[index:index+1])
-        print("We predict: ")
-        print(pred[0])
-        print("Was supposed to be: ")
-        print(y_train[index])
-        print("Proba: ")
-        print(pred_proba)
-        print()
-
-        f0 = plt.figure(1)
-        plt.bar(np.arange(10), pred_proba[0])
-        f0.show()
-
-        f1 = plt.figure(2)
-        ex = np.reshape(X_train[index:index+1], (28,28))
-        plt.imshow(ex)
-        f1.show()
-        input()
-        plt.close("all")
+    '''
